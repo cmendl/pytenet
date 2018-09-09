0f33bf6d54da49fc7f75c42031599b283c991e40
Christian B. Mendl <christian.b.mendl@gmail.com>
2018-09-09 19:15:12 +0200

    extend SVD splitting of a matrix to take quantum numbers into account

--- pytenet/bond_ops.py
+++ pytenet/bond_ops.py
@@ -19,14 +19,103 @@ def retained_bond_indices(s, tol):
     return np.where(s > tol)[0]
 
 
-def split_matrix_svd(A, tol):
+def split_matrix_svd(A, q0, q1, tol):
     """
     Split a matrix by singular value decomposition,
+    taking block sparsity structure dictated by quantum numbers into account,
     and truncate small singular values based on tolerance.
     """
-    u, s, v = np.linalg.svd(A, full_matrices=False)
+    assert A.ndim == 2
+    assert len(q0) == A.shape[0]
+    assert len(q1) == A.shape[1]
+
+    # find common quantum numbers
+    qis = np.intersect1d(q0, q1)
+
+    if len(qis) == 0:
+        assert np.linalg.norm(A) == 0
+        # special case: no common quantum numbers;
+        # use dummy intermediate dimension 1
+        u = np.zeros((A.shape[0], 1), dtype=A.dtype)
+        v = np.zeros((1, A.shape[1]), dtype=A.dtype)
+        s = np.zeros(1)
+        # single column of 'u' should have norm 1
+        u[0, 0] = 1
+        # ensure non-zero entry in 'u' formally matches quantum numbers
+        q = q0[:1]
+        # 'v' must remain zero matrix to satisfy quantum number constraints
+        return (u, s, v, q)
+
+    # require NumPy arrays for indexing
+    q0 = np.array(q0)
+    q1 = np.array(q1)
+
+    # sort quantum numbers and arrange entries in A accordingly;
+    # using mergesort to avoid permutations of identical quantum numbers
+    idx0 = np.argsort(q0, kind='mergesort')
+    idx1 = np.argsort(q1, kind='mergesort')
+    if np.any(idx0 - np.arange(len(idx0))):
+        # if not sorted yet...
+        q0 = q0[idx0]
+        A = A[idx0, :]
+    if np.any(idx1 - np.arange(len(idx1))):
+        # if not sorted yet...
+        q1 = q1[idx1]
+        A = A[:, idx1]
+
+    # maximum intermediate dimension
+    max_interm_dim = min(A.shape)
+
+    # keep track of intermediate dimension
+    D = 0
+
+    u = np.zeros((A.shape[0], max_interm_dim), dtype=A.dtype)
+    v = np.zeros((max_interm_dim, A.shape[1]), dtype=A.dtype)
+    s = np.zeros(max_interm_dim)
+
+    # corresponding intermediate quantum numbers
+    q = np.zeros(max_interm_dim, dtype=q0.dtype)
+
+    # for each shared quantum number...
+    for qn in qis:
+        # indices of current quantum number
+        iqn = np.where(q0 == qn)[0]; i0 = iqn[0]; i1 = iqn[-1] + 1
+        iqn = np.where(q1 == qn)[0]; j0 = iqn[0]; j1 = iqn[-1] + 1
+
+        # perform SVD decomposition of current block
+        usub, ssub, vsub = np.linalg.svd(A[i0:i1, j0:j1], full_matrices=False)
+
+        # update intermediate dimension
+        Dprev = D
+        D += len(ssub)
+
+        u[i0:i1, Dprev:D] = usub
+        v[Dprev:D, j0:j1] = vsub
+        s[Dprev:D] = ssub
+        q[Dprev:D] = qn
+
+    assert D <= max_interm_dim
+
+    # use actual intermediate dimensions
+    u = u[:, :D]
+    v = v[:D, :]
+    s = s[:D]
+    q = q[:D]
+
+    # truncate small singular values
     idx = retained_bond_indices(s, tol)
-    return (u[:, idx], s[idx], v[idx, :])
+    u = u[:, idx]
+    v = v[idx, :]
+    s = s[idx]
+    q = q[idx]
+
+    # undo sorting of quantum numbers
+    if np.any(idx0 - np.arange(len(idx0))):
+        u = u[np.argsort(idx0), :]
+    if np.any(idx1 - np.arange(len(idx1))):
+        v = v[:, np.argsort(idx1)]
+
+    return (u, s, v, q)
 
 
 def qr(A, q0, q1):
@@ -61,9 +150,10 @@ def qr(A, q0, q1):
     q0 = np.array(q0)
     q1 = np.array(q1)
 
-    # sort quantum numbers and arrange entries in A accordingly
-    idx0 = np.argsort(q0)
-    idx1 = np.argsort(q1)
+    # sort quantum numbers and arrange entries in A accordingly;
+    # using mergesort to avoid permutations of identical quantum numbers
+    idx0 = np.argsort(q0, kind='mergesort')
+    idx1 = np.argsort(q1, kind='mergesort')
     if np.any(idx0 - np.arange(len(idx0))):
         # if not sorted yet...
         q0 = q0[idx0]
--- test/test_bond_ops.py
+++ test/test_bond_ops.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import sys
 sys.path.append('../pytenet/')
-from bond_ops import qr
+from bond_ops import qr, split_matrix_svd
 from qnumber import qnumber_outer_sum, is_qsparse
 
 
@@ -33,6 +33,49 @@ class TestBondOps(unittest.TestCase):
                         msg='sparsity pattern of Q matrix must match quantum numbers')
         self.assertTrue(is_qsparse(R, [qinterm, -q1]),
                         msg='sparsity pattern of R matrix must match quantum numbers')
+
+
+    def test_split_matrix_svd(self):
+
+        A = randn_complex((17, 26))
+
+        # fictitious quantum numbers
+        q0 = np.random.randint(-2, 3, size=A.shape[0])
+        q1 = np.random.randint(-2, 3, size=A.shape[1])
+
+        # enforce block sparsity structure dictated by quantum numbers
+        mask = qnumber_outer_sum([q0, -q1])
+        A = np.where(mask == 0, A, 0)
+
+        # perform SVD decomposition without truncation
+        u, s, v, qinterm = split_matrix_svd(A, q0, q1, 0.)
+
+        self.assertAlmostEqual(np.linalg.norm(np.dot(u * s, v) - A), 0.,
+                               delta=1e-13, msg='U.S.V must match A matrix')
+
+        self.assertAlmostEqual(np.linalg.norm(np.dot(u.conj().T, u) - np.identity(u.shape[1])), 0.,
+                               delta=1e-14, msg='columns of U matrix must be orthonormalized')
+
+        self.assertTrue(is_qsparse(u, [q0, -qinterm]),
+                        msg='sparsity pattern of U matrix must match quantum numbers')
+        self.assertTrue(is_qsparse(v, [qinterm, -q1]),
+                        msg='sparsity pattern of V matrix must match quantum numbers')
+
+        s_norm = np.linalg.norm(s)
+
+        # perform SVD decomposition with truncation
+        u, s, v, qinterm = split_matrix_svd(A, q0, q1, 0.15)
+
+        self.assertAlmostEqual(np.linalg.norm(np.dot(u * s, v) - A), np.sqrt(s_norm**2 - np.linalg.norm(s)**2),
+                               delta=1e-14, msg='weight of truncated singular values must agree with norm of matrix difference')
+
+        self.assertAlmostEqual(np.linalg.norm(np.dot(u.conj().T, u) - np.identity(u.shape[1])), 0.,
+                               delta=1e-14, msg='columns of U matrix must be orthonormalized')
+
+        self.assertTrue(is_qsparse(u, [q0, -qinterm]),
+                        msg='sparsity pattern of U matrix must match quantum numbers')
+        self.assertTrue(is_qsparse(v, [qinterm, -q1]),
+                        msg='sparsity pattern of V matrix must match quantum numbers')
 
 
 def randn_complex(size):
