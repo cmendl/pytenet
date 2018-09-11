5430c6a0ffb771119cec758344ec2757978ef6ac
Christian B. Mendl <christian.b.mendl@gmail.com>
2018-09-11 17:35:23 +0200

    update operation test with quantum numbers

--- test/test_operation.py
+++ test/test_operation.py
@@ -14,13 +14,21 @@ class TestOperation(unittest.TestCase):
         # physical dimension
         d = 3
 
+        # physical quantum numbers
+        qd = np.random.randint(-1, 2, size=d)
+
         # create random matrix product state
-        D = [1, 7, 6, 4, 5, 8, 1]
-        psi = MPS(d, D, fill='random')
+        D = [1, 7, 26, 19, 25, 8, 1]
+        psi = MPS(qd, [np.random.randint(-1, 2, size=Di) for Di in D], fill='random')
+        # rescale to achieve norm of order 1
+        for i in range(psi.nsites):
+            psi.A[i] *= 5
 
         # create random matrix product operator
-        D = [1, 5, 6, 4, 7, 4, 1]
-        op = MPO(d, D=D, fill='random')
+        D = [1, 5, 16, 14, 17, 4, 1]
+        # set bond quantum numbers to zero since otherwise,
+        # sparsity pattern often leads to <psi | op | psi> = 0
+        op = MPO(qd, qD=[np.zeros(Di) for Di in D], fill='random')
 
         # calculate average (expectation value) <psi | op | psi>
         avr = operator_average(psi, op)
@@ -30,7 +38,7 @@ class TestOperation(unittest.TestCase):
         avr_ref = np.vdot(x, np.dot(op.as_matrix(), x))
 
         # relative error
-        err = abs(avr - avr_ref) / abs(avr_ref)
+        err = abs(avr - avr_ref) / max(abs(avr_ref), 1e-12)
         self.assertAlmostEqual(err, 0., delta=1e-12, msg='operator average must match reference value')
 
 
